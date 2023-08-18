259e15f9f6284d55f2f8c0ef9706bc87a2e44972
TheMetaSetter <134222962+TheMetaSetter@users.noreply.github.com>
2023-08-18 21:19:19 +0700

    Use bit array to determine level of a new node

---
+++ build/CMakeFiles/SkipList.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SkipList.dir/SkipList.cpp.o"
+  "CMakeFiles/SkipList.dir/SkipList.cpp.o.d"
+  "CMakeFiles/SkipList.dir/main.cpp.o"
+  "CMakeFiles/SkipList.dir/main.cpp.o.d"
+  "SkipList"
+  "SkipList.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SkipList.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
